550fa5fcbc37b981bb7b830a654adccf9cf5b5b9
Berkanadem <berkanadem2507@gmail.com>
2023-02-15 16:30:53 +0100

    first commit

---
+++ homework1/homework1.js
@@ -0,0 +1,24 @@
+let name = "Berkan Adem";
+let age = 20;
+let job = "Konobar"
+let gender = "male"
+let livesInSkopje = true;
+
+
+let person = {
+    name:"Berkan Adem",
+    age: "20",
+    job: "Konobar",
+    gender: "male",
+    liveInSkopje: true,
+    
+
+}
+console.log(person);
+
+alert(person.name + " " + person.age + " "  + person.job + " " + person.gender + " " + person.liveInSkopje)
+
+function person1(){
+    alert(person.name + " " + person.age + " "  + person.job + " " + person.gender + " " + person.liveInSkopje);
+    
+}
---
+++ homework2/homework2.js
@@ -0,0 +1,15 @@
+let birthday = 25;
+let fullName = 10;
+let birthYear = 99;
+let yearSum = 1 + 9 + 9 + 9;
+
+let firstCalc = birthday > fullName;
+let secondCalc = birthday % yearSum;
+
+console.log(firstCalc);
+let thirClac = " Jas sum ";
+if(firstCalc=== true && secondCalc < 4) {
+    console.log(thirClac + "light");
+} else {
+    console.log(thirClac + "darkness");
+}
